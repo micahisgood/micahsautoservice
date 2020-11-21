bf2918543799bcf2367a6054f6e0247291d2225c
MICAH LEE <TEDDYJAMES54@gmail.com>
2020-11-21 14:05:57 -0800

    javascript adds

--- c01.js/app.js
+++ c01.js/app.js
@@ -1,8 +1,36 @@
-var userName = prompt('enter your name?');
+'use strict'
+
+function askUserNameQuestion(){
+    var userName = prompt('enter your name?');
+    document.write(userName);
+}
+
+function askUserNameQuestion(){
+    var questionAnswer = prompt('do you own domestic or international?')
+    document.write(questionAnswer);
+}
+
+    if (userName === 'lord of the manner'){
+    document.write(`Welcome Honored Guest + {userName}`)
+    } else {
+    document.write(`Welcome + {userName}`);
+    
+} 
 
 
-if (userName == 'lord of the manner'){
-    document.write(`Welcome Honored Guest  ${userName}`)
+
+var askUserNameQuestion2= confrim('how long have you owned it?');
+if (confirmAnswer){
+    alert('domestic or international');
 } else {
-    document.write(`Welcome ${userName}`);
+    alert('we are experts in both!')
+}
+
+var userName1 = askUserNameQuestion();
+console.log(userName1);
+
+if (userName1=== 'international'){
+    document.write('Welcome international'+ userName1)
+} else {
+    document.write('Welcome Domestic' + userName1);
 }
